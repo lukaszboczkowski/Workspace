98be3236cdc7a178ab0446401c6ac36de2fc3ed2
lukaszboczkowski <45945596+lukaszboczkowski@users.noreply.github.com>
2019-01-17 20:29:49 +0100

    Create Main.java

---
+++ TheGame/src/TheGame/Main.java
@@ -0,0 +1,22 @@
+package TheGame;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		Plansza plansza = new Plansza();
+		
+		obj.setBounds(10, 01, 905, 700);
+		obj.setBackground(Color.DARK_GRAY);
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(plansza);
+	
+		
+}
+}
